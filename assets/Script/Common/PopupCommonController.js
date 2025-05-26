c8b0d21c87bc5bdb4f71cdb999df3f34e12fe598
khoa_bt <68lktrongkhoa@gmail.com>
2025-05-26 12:45:55 +0700

    feature/btPopupDay3 | clean comment

--- assets/Script/Common/PopupCommonController.js
+++ assets/Script/Common/PopupCommonController.js
@@ -31,19 +31,6 @@ cc.Class({
             type: cc.String,
             tooltip: "Tên của animation clip để chạy khi đóng (nếu có Close Animation)."
         },
-
-        // Tùy chọn: Có đóng popup khi click ra ngoài vùng popup không?
-        // Yêu cầu popup có một Node Background với cc.BlockInputEvents
-        // closeOnTouchOutside: {
-        //     default: false,
-        //     tooltip: "Đóng popup khi click ra ngoài? (Yêu cầu có background chặn input)"
-        // },
-        // backgroundNodeForTouchOutside: {
-        //     default: null,
-        //     type: cc.Node,
-        //     tooltip: "Node background để bắt sự kiện click bên ngoài (nếu closeOnTouchOutside=true)",
-        //     visible: function() { return this.closeOnTouchOutside; }
-        // }
     },
 
     onLoad () {
@@ -64,17 +51,6 @@ cc.Class({
                 }
             });
         }
-
-        // (Tùy chọn: Xử lý closeOnTouchOutside)
-        // if (this.closeOnTouchOutside && this.backgroundNodeForTouchOutside) {
-        //     // Thêm một component cc.BlockInputEvents vào backgroundNodeForTouchOutside nếu chưa có
-        //     // và lắng nghe sự kiện 'touchstart' hoặc 'mousedown' trên nó
-        //     // Trong handler, gọi this.hidePopup();
-        //     // Quan trọng: Đảm bảo background này là con trực tiếp của popupNode
-        //     // và các element tương tác được nằm phía trên nó trong thứ tự render.
-        //     // Ví dụ:
-        //     // this.backgroundNodeForTouchOutside.on(cc.Node.EventType.TOUCH_START, this.hidePopup, this);
-        // }
     },
 
     showPopup(data) {
@@ -89,15 +65,6 @@ cc.Class({
             if (this.openAnimation && this.openAnimationClipName) {
                 this.openAnimation.play(this.openAnimationClipName);
             }
-            // Hoặc hiệu ứng đơn giản:
-            // this.popupNode.scale = 0.8;
-            // this.popupNode.opacity = 0;
-            // cc.tween(this.popupNode)
-            //     .to(0.3, { scale: 1, opacity: 255 }, { easing: 'sineOut' })
-            //     .start();
-
-            // Gọi một hàm tùy chỉnh trên popup nếu có, để xử lý dữ liệu được truyền vào
-            // Ví dụ: một script khác trên cùng popupNode có hàm `setup(data)`
             let specificController = this.getComponent('SpecificPopupLogic');
             if (specificController && typeof specificController.setup === 'function') {
                 specificController.setup(data);
@@ -120,11 +87,6 @@ cc.Class({
                 this.closeAnimation.once('finished', doHide, this);
                 this.closeAnimation.play(this.closeAnimationClipName);
             } else {
-                // Hoặc hiệu ứng đơn giản:
-                // cc.tween(this.popupNode)
-                //     .to(0.3, { scale: 0.8, opacity: 0 }, { easing: 'sineIn' })
-                //     .call(doHide)
-                //     .start();
                 doHide();
             }
         }
